b094680f17781ff3420c5db63cb2a639c064e43a
123d-com <56535213+123d-com@users.noreply.github.com>
2020-02-07 18:48:03 +0800

    Add files via upload

---
+++ loving.py
@@ -0,0 +1,29 @@
+#coding:gbk
+"""
+第一个小项目：粉红爱心
+作者：李建均
+日期：2020/02/07
+"""
+
+import turtle
+def yy():
+	for i in range(200):
+	    turtle.right(1)
+	    turtle.fd(1)	 
+turtle.pensize(8)
+turtle.speed(10)
+turtle.pencolor("pink")
+turtle.fillcolor("pink")
+turtle.begin_fill()
+turtle.left(140)
+turtle.fd(110)
+yy()
+turtle.left(120)
+yy()
+turtle.fd(110)
+turtle.end_fill()
+turtle.color("pink")
+turtle.goto(20,165)
+turtle.write("李建均爱杨洋",font=('1437', 30, 'normal')) 
+turtle.mainloop()
+ 
